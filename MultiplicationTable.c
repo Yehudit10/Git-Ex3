29c485e23a992e008dd4236d55b0da9b376162de
Yehudit10 <yehudit50402@gmail.com>
2024-11-24 18:33:37 +0200

    Create MultiplicationTable.c

---
+++ MultiplicationTable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    printf("\n     ");
+    for (i = 1; i <= 10; i++) {
+        printf("%4d", i);
+    }
+    printf("\n");
+    for (i = 1; i <= 10; i++) {
+        printf("%4d", i);
+        for (j = 1; j <= 10; j++) {
+            printf("%4d", i * j); 
+        }
+        printf("\n"); 
+    }
+}
